cf9c308db1fecdceae36b712e9215447a36f70c2
everybees <everybees@gmail.com>
2022-01-09 23:11:43 +0100

    added heroku db

--- wollet/settings.py
+++ wollet/settings.py
@@ -101,27 +101,19 @@ WSGI_APPLICATION = 'wollet.wsgi.application'
 AUTH_USER_MODEL = 'api.User'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': env('DB_NAME'),
+#         'USER': env('DB_USER'),
+#         'PASSWORD': env('DB_PASSWORD'),
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
 #     }
 # }
 
-
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': env('DB_NAME'),
-        'USER': env('DB_USER'),
-        'PASSWORD': env('DB_PASSWORD'),
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-    }
+    'default': env('DATABASE_URL', cast=dj_database_url.parse),
 }
 
 
