0dee91d663e1f5e421f37536a405d5ea1d999345
everybees <everybees@gmail.com>
2022-01-09 12:31:35 +0100

    added gunicorn

--- wollet/settings.py
+++ wollet/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url 
 import environ
 import os
 
@@ -173,3 +174,6 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'wollet/static')]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+prod_db  =  dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
